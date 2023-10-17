7d07d78027dc02b53c6bb0158ab60591b831b99f
1ouiss <ls.bouet@gmail.com>
2023-10-17 13:28:50 +0200

    init project with nextUi

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/react";
+import { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
+};
+
+export default Index;
