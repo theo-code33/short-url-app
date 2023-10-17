48e97ff04894e76db4a748e7b23cc9f00fd06d0b
1ouiss <ls.bouet@gmail.com>
2023-10-17 22:17:53 +0200

    add redirect if token exist

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,19 @@
 import FormConnect from "@/components/FormConnect";
+import { Token } from "@/types";
+import { getUserFromLocalToken } from "@/utils/jwt";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Admin = () => {
+  const router = useRouter();
+  useEffect(() => {
+    const token = getUserFromLocalToken() as Token;
+    if (token !== null) {
+      router.push("/admin/dashboard");
+      return;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="flex aligns-center w-full justify-center h-screen">
       <FormConnect />
