50dad7f5b3aba3a3fe029114f5700e8c9a9e6931
1ouiss <ls.bouet@gmail.com>
2023-10-18 10:17:20 +0200

    add url api in .env

--- pages/api/instance.ts
+++ pages/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/v0",
+  baseURL: process.env.NEXT_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
