eec7274b11248e54af0f236b41c508b9b2817914
1ouiss <ls.bouet@gmail.com>
2023-10-19 09:34:55 +0200

    secure updateUser route

--- pages/api/updateUser.ts
+++ pages/api/updateUser.ts
@@ -3,7 +3,9 @@ import instance from "./instance";
 
 const updateUser = async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
-  const response = await instance.patch(`/user/${id}`, req.body);
+  const response = await instance.patch(`/user/${id}`, req.body, {
+    headers: req.headers,
+  });
   res.status(200).json(response.data);
 };
 
