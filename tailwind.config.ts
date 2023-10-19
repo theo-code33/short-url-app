5aa333b923b9d5020047dd4d447ab2a0f163b1dc
1ouiss <ls.bouet@gmail.com>
2023-10-19 13:52:42 +0200

    update tailwind and tailwind config to apply style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,10 @@ const { nextui } = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // ...
+    "./src/**/*.{html,js}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
